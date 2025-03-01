b2d044a5b58ba5b9d8a8852846d90d0e3d9dac68
Mal <44848986+malachiperrin@users.noreply.github.com>
2025-03-01 00:28:44 -0500

    User and Admin Routes Implemented

--- app.js
+++ app.js
@@ -3,21 +3,22 @@ const path = require('path');
 const { mongoConnect, getDb } = require('./utils/database');
 const bodyParser = require('body-parser');
 
+const routes = require('./routes/routes');
+const adminRoutes = require('./routes/admin/adminRoutes');
+const userRoutes = require('./routes/user/userRoutes');
 
 
 const app = new express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-const routes = require('./routes/routes');
-
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
+app.use('/admin', adminRoutes);
+app.use('/user', userRoutes);
 
 
 mongoConnect(app.listen(process.env.PORT));
